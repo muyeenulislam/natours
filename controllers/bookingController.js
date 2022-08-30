ebb57b85b8bd838a607c3f53b8bd84fe6ffb012a
muyeen <muyeenulislamsakif@gmail.com>
2022-08-30 13:57:40 +0600

    new commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -61,8 +61,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.dsiplay_items[0].price_data.unit_amount / 100;
-  console.log(tour);
+  const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
