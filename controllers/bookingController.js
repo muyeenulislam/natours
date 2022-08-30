8dc26eb55324295d29a3be26b5349f2d95a626a3
muyeen <muyeenulislamsakif@gmail.com>
2022-08-30 13:22:32 +0600

    Implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = client_reference_id;
+  const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
