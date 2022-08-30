a306e35fe912be19c9f2f4d74fd877a3fd4aa4c8
muyeen <muyeenulislamsakif@gmail.com>
2022-08-30 14:14:29 +0600

    new commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,6 +62,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
+  console.log(tour, price, user);
   await Booking.create({ tour, user, price });
 };
 
@@ -79,7 +80,6 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  console.log(event);
   if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
