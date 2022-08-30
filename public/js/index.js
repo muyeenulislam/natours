746e65e836120fbd2c2f611c20b42af07f0b136e
muyeen <muyeenulislamsakif@gmail.com>
2022-08-30 13:17:29 +0600

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -73,4 +73,4 @@ if (bookBtn)
 
 const alertMessage = document.querySelector('body').dataset.alert;
 
-if (alert) showAlert('success', alertMessage, 15);
+if (alertMessage) showAlert('success', alertMessage, 15);
