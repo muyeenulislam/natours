c586dc227bd3620f9755ae705f62507aadd8dfab
muyeen <muyeenulislamsakif@gmail.com>
2022-08-30 13:46:46 +0600

    Implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
     // cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
